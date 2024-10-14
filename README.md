adef70e3ddc9e9f816ed5c16eed68de923064587
AryanBhardwaj789 <aryanbhardwaj98126@gmail.com>
2024-10-14 18:52:26 -0400

    Project Completed

---
+++ README.md
@@ -0,0 +1,10 @@
+/ - login and register screen ---
+/register ---
+/login ---
+/profile - profile page with boards
+/feed - feed page with all different pins
+/save/:pinid - save pin to a board
+/delete/:pinid - delete pin from a board
+/logout
+/edit
+/upload
---
+++ routes/users.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const mongoose = require('mongoose');
+const plm = require("passport-local-mongoose");
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(() => console.log("MongoDB connected!"))
+.catch(err => console.error("MongoDB connection error:", err));
+
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  contact: Number,
+  boards: [],
+  posts: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "post"
+  }]
+});
+
+userSchema.plugin(plm); 
+
+module.exports = mongoose.model("user", userSchema);
